7b26cc15e84187aba766614df41e6f9a53650ca6
zzun <736300@naver.com>
2022-12-31 21:34:13 +0900

    feat: Spring Data JPA를 사용한 회원 레포지토리 기능 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -13,24 +13,39 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
-    @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
-    }
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository()); /** 파라미터 정보 확인: Ctrl + p */
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
+    // 스프링 데이터 JPA 사용 안 할 경우
+//    private final DataSource dataSource;
+//    private final EntityManager em;
+//    @Autowired
+//    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.dataSource = dataSource;
+//        this.em = em;
+//    }
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository()); /** 파라미터 정보 확인: Ctrl + p */
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource); // Jdbc 사용시 (OCP)
 //        return new JdbcTemplateMemberRepository(dataSource); // JdbcTemplate 사용시 (순수 Jdbc API 반복 코드를 대부분 제거, SQL는 직접 작성 필요)
-        return new JpaMemberRepository(em);
+//        return new JpaMemberRepository(em); // JPA 사용시
+//    }
+
+
+    // 스프링 데이터 JPA 사용할 경우
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository); /** 파라미터 정보 확인: Ctrl + p */
+    }
+
+
 }
